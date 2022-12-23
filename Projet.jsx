a82ab5f234348a6528c44f09b00353256dd35806
mayoufij123 <mayouffj@gmail.com>
2022-12-23 23:04:39 +0100

    test validation task

---
+++ Projet.jsx
@@ -0,0 +1,10 @@
+var ckliked=document.getElementsByClassName("mycheck")
+console.log(ckliked)
+var change=document.getElementsByClassName("change")
+console.log(change)
+ for(let i=0;i<change.length;i++){
+  ckliked[i].addEventListener('click', function() {
+    change[i].innerHTML='✔';
+
+})
+ }
